3ef765a5fec4b0da395adc5dda4d94c0431382ce
dancingshell <kaitlinjaffe@gmail.com>
2014-09-02 22:21:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ipsum::Application.config.secret_key_base = 'b78655702cc5b9ebbcc1c6ed017d5aaf3906d3d08ef03c3d04985a511d19f56c6d2233f06cc8abc8bda7d685b514dd311f16ea27b64dcc20a46759f9466313be'
